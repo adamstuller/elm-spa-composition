93671d2dedc4942baaf638b8df16b9b6bb85423a
Adam Štuller <adam.stuller@protonmail.com>
2021-11-11 22:11:58 +0100

    pages composition

--- example/Router.elm
+++ example/Router.elm
@@ -1,8 +1,12 @@
 module Router exposing (..)
 
-import Browser
+import AltComposition exposing (Both)
+import Browser exposing (UrlRequest, application)
 import Browser.Navigation as Nav
-import Html exposing (Html)
+import Either exposing (Either(..))
+import Html exposing (Html, b)
+import List.Nonempty as NE
+import Page exposing (ApplicationWithRouter, PageWidgetComposed, Subscription, Update, View, initWith, initWithRouter, subscribeWith, updateWith)
 import Url
 
 
@@ -39,9 +43,39 @@ update msg model =
             )
 
 
-pageWidget =
-    { init = init
-    , update = update
-    , onUrlChange = UrlChanged
-    , onUrlRequest = LinkClicked
+subscriptions : Subscription Model Msg
+subscriptions model =
+    Sub.none
+
+
+initRouter :
+    PageWidgetComposed model msg flags path
+    -> ApplicationWithRouter (Both model Model) (Either msg Msg) flags
+initRouter w =
+    let
+        viewPaths =
+            \_ ->
+                { title = "SPA"
+                , body =
+                    [ Html.div []
+                        [ Html.ul [] <|
+                            NE.toList <|
+                                NE.map
+                                    (\( path, route ) ->
+                                        Html.li []
+                                            [ Html.text <| route ++ "  "
+                                            , Html.text <| Debug.toString path
+                                            ]
+                                    )
+                                    w.routingRules
+                        ]
+                    ]
+                }
+    in
+    { init = initWithRouter w.init init
+    , view = viewPaths
+    , update = updateWith w.update update
+    , subscriptions = subscribeWith w.subscriptions subscriptions
+    , onUrlChange = \url -> Right <| UrlChanged url
+    , onUrlRequest = \urlRequest -> Right <| LinkClicked urlRequest
     }
