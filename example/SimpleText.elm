b53df2097e197f1c0883fb78ac875834bb43b405
Adam Štuller <adam.stuller@protonmail.com>
2021-11-13 13:56:08 +0100

    formatting

--- example/SimpleText.elm
+++ example/SimpleText.elm
@@ -4,8 +4,8 @@ import AltComposition.Pure exposing (View)
 import Html exposing (..)
 import Json.Decode exposing (Decoder, decodeValue, field, string)
 import Page exposing (PageWidget, Route)
-import Widget exposing (Widget)
 import Router exposing (Flags)
+import Widget exposing (Widget)
 
 
 flagsDecoder : Decoder String
@@ -45,18 +45,18 @@ initWidget s =
 
 
 initPageWidget : Route -> String -> PageWidget Model Msg Flags
-initPageWidget route s =
+initPageWidget route defaultText =
     let
         updateEffectfull =
             \msg model -> ( update msg model, Cmd.none )
 
         initWithFlags f =
             case decodeValue flagsDecoder f of
-                Ok decodedString ->
-                    (decodedString, Cmd.none)
+                Ok decodedText ->
+                    ( decodedText, Cmd.none )
 
                 Err _ ->
-                    (s, Cmd.none)
+                    ( defaultText, Cmd.none )
     in
     { init = ( initWithFlags, route )
     , update = updateEffectfull
