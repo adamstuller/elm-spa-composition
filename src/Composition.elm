c67b270903525ef9934d4341f3406d55d9f4572e
Adam Štuller <adam.stuller@protonmail.com>
2021-12-07 18:55:28 +0100

    cleaning continues

--- src/Composition.elm
+++ src/Composition.elm
@@ -1,21 +1,9 @@
 module Composition exposing (..)
 
-import Common exposing (Both, Route)
+import Common exposing (Both, Route, View, Update, Subscription)
 import Either exposing (Either(..))
 import Html exposing (Html)
 import List.Nonempty as NE exposing (Nonempty)
-
-
-type alias Subscription model msg =
-    model -> Sub msg
-
-
-type alias Update model msg =
-    msg -> model -> ( model, Cmd msg )
-
-
-type alias View model msg =
-    model -> Html msg
 
 
 {-| Combines two inits models to selector and list of paths.
@@ -65,52 +53,6 @@ orInit ( next, ps, routes ) ( init2, route2 ) =
     )
 
 
-updateEither :
-    Update model1 msg1
-    -> Update model2 msg2
-    -> Update (Either model1 model2) (Either msg1 msg2)
-updateEither u1 u2 msg model =
-    case ( msg, model ) of
-        ( Left msg1, Left model1 ) ->
-            Tuple.mapBoth Left (Cmd.map Left) <| u1 msg1 model1
-
-        ( Right msg2, Right model2 ) ->
-            Tuple.mapBoth Right (Cmd.map Right) <| u2 msg2 model2
-
-        _ ->
-            ( model, Cmd.none )
-
-
-{-| Combines two subscriptions.
--}
-subscribeEither :
-    Subscription model1 msg1
-    -> Subscription model2 msg2
-    -> Subscription (Either model1 model2) (Either msg1 msg2)
-subscribeEither s1 s2 model =
-    case model of
-        Left model1 ->
-            Sub.map Left <| s1 model1
-
-        Right model2 ->
-            Sub.map Right <| s2 model2
-
-
-{-| Combines two views into view for the SUM of models.
--}
-eitherView :
-    View model1 msg1
-    -> View model2 msg2
-    -> View (Either model1 model2) (Either msg1 msg2)
-eitherView v1 v2 m =
-    case m of
-        Left m1 ->
-            Html.map Left <| v1 m1
-
-        Right m2 ->
-            Html.map Right <| v2 m2
-
-
 initWith :
     (flags -> ( model1, Cmd msg1 ))
     -> (flags -> ( model2, Cmd msg2 ))
@@ -129,6 +71,23 @@ initWith init1 init2 flags =
         , Cmd.map Right c2
         ]
     )
+
+--- Update Composition
+
+updateEither :
+    Update model1 msg1
+    -> Update model2 msg2
+    -> Update (Either model1 model2) (Either msg1 msg2)
+updateEither u1 u2 msg model =
+    case ( msg, model ) of
+        ( Left msg1, Left model1 ) ->
+            Tuple.mapBoth Left (Cmd.map Left) <| u1 msg1 model1
+
+        ( Right msg2, Right model2 ) ->
+            Tuple.mapBoth Right (Cmd.map Right) <| u2 msg2 model2
+
+        _ ->
+            ( model, Cmd.none )
 
 
 {-| Updates one of two submodels using corresponding subupdate function.
@@ -150,6 +109,10 @@ updateWith u1 u2 =
     Either.unpack (applyL u1) (applyR u2)
 
 
+
+--- Subscribe composition
+
+
 {-| Combines two subscriptions.
 -}
 subscribeWith :
@@ -163,20 +126,35 @@ subscribeWith s1 s2 ( m1, m2 ) =
         ]
 
 
-viewWith : View model2 msg2 -> (model1 -> List (Html msg1)) -> Both model1 model2 -> List (Html (Either msg1 msg2))
-viewWith view2 hs ( model1, model2 ) =
-    List.append (List.map (Html.map Left) <| hs model1) [ Html.map Right <| view2 model2 ]
+{-| Combines two subscriptions.
+-}
+subscribeEither :
+    Subscription model1 msg1
+    -> Subscription model2 msg2
+    -> Subscription (Either model1 model2) (Either msg1 msg2)
+subscribeEither s1 s2 model =
+    case model of
+        Left model1 ->
+            Sub.map Left <| s1 model1
+
+        Right model2 ->
+            Sub.map Right <| s2 model2
 
 
-viewBoth : View model1 msg1 -> View model2 msg2 -> Both model1 model2 -> Both (Html (Either msg1 msg2)) (Html (Either msg1 msg2))
-viewBoth view1 view2 ( model1, model2 ) =
-    ( Html.map Left <| view1 model1, Html.map Right <| view2 model2 )
+
+--- View composition
 
 
-composeViews : View model1 msg1 -> View model2 msg2 -> Both model1 model2 -> Html (Either msg1 msg2)
-composeViews view1 view2 ( model1, model2 ) =
-    let
-        ( h1, h2 ) =
-            viewBoth view1 view2 ( model1, model2 )
-    in
-    Html.div [] [ h1, h2 ]
+{-| View composition expecting tuple of models
+-}
+viewEither :
+    View model1 msg1
+    -> View model2 msg2
+    -> View (Either model1 model2) (Either msg1 msg2)
+viewEither v1 v2 m =
+    case m of
+        Left m1 ->
+            Html.map Left <| v1 m1
+
+        Right m2 ->
+            Html.map Right <| v2 m2
--- src/Page.elm
+++ src/Page.elm
@@ -3,7 +3,7 @@ module Page exposing (..)
 import Browser exposing (Document, UrlRequest)
 import Browser.Navigation as Navigation
 import Common exposing (Both, View, Update, Subscription)
-import Composition exposing (eitherView, oneOfInits, orInit, subscribeEither, updateEither)
+import Composition exposing (viewEither, oneOfInits, orInit, subscribeEither, updateEither)
 import Either exposing (Either(..))
 import Html exposing (Html)
 import List.Nonempty as NE exposing (Nonempty)
@@ -43,7 +43,7 @@ type alias ApplicationWithRouter model msg flags =
 join : PageWidget model1 msg1 flags -> PageWidget model2 msg2 flags -> PageWidgetComposition (Either model1 model2) (Either msg1 msg2) (Either () ()) flags
 join w1 w2 =
     { init = oneOfInits w1.init w2.init
-    , view = eitherView w1.view w2.view
+    , view = viewEither w1.view w2.view
     , update = updateEither w1.update w2.update
     , subscriptions = subscribeEither w1.subscriptions w2.subscriptions
     }
@@ -52,7 +52,7 @@ join w1 w2 =
 add : PageWidgetComposition model1 msg1 path flags -> PageWidget model2 msg2 flags -> PageWidgetComposition (Either model1 model2) (Either msg1 msg2) (Either path ()) flags
 add w1 w2 =
     { init = orInit w1.init w2.init
-    , view = eitherView w1.view w2.view
+    , view = viewEither w1.view w2.view
     , update = updateEither w1.update w2.update
     , subscriptions = subscribeEither w1.subscriptions w2.subscriptions
     }
